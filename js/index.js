b4ed3ace69cbe088bde99cb269540612c7a3ab5b
laizuakram1 <laizuakram98@gmail.com>
2022-02-14 01:24:11 +0600

    more dynamic pin matching system

--- js/index.js
+++ js/index.js
@@ -25,7 +25,29 @@ document.getElementById('key-pad').addEventListener('click', function (event){
    const previousNumbers = inputNumbers.value;
    inputNumbers.value = previousNumbers + keyNumbers;
    if(keyNumbers == 'C' || keyNumbers == '<'){
-       inputNumbers.value = '';
+    inputNumbers.value = '';
    }
    
-})
+   
+});
+
+// code matching with a function 
+function matchCode(){
+    const generatePin = document.getElementById('generate-pin-input').value;
+    const keyNumbers = document.getElementById('input-numbers').value;
+
+    const matched = document.getElementById('matched-notify');
+    const unmatched = document.getElementById('unmatched-notify');
+   
+    if(keyNumbers == generatePin){
+        matched.style.display = 'block';
+        unmatched.style.display = 'none';
+       
+       
+    }
+    else{
+        unmatched.style.display = 'block';
+        matched.style.display = 'none';  
+    }
+    
+};
